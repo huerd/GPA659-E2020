782e0f1ec8b834ea50cda9d47fa8ce8774d6681e
huerd <huerd@users.noreply.github.com>
2020-05-27 19:06:13 -0400

    - fixed imageNegative

--- imageNegative.m
+++ imageNegative.m
@@ -1,6 +1,13 @@
+% Fonction 16
+% Reçoit une image f (type uint8) en paramètre, la convertit en niveau de gris si nécessaire,
+% applique une transformée qui génère une image négative (donc s=L-1-r, avec L étant le
+% nombre de niveaux r la valeur du pixel d'origine et s la valeur du pixel inversé). La nouvelle
+% image est retournée
 function [g] = imageNegative(f)
-%IMAGENEGATIVE Summary of this function goes here
-%   Detailed explanation goes here
-    g=255-rgb2gray(f);
+    % converts to greyscale if RGB
+    if (size(f,3) == 3)
+        f = rgb2gray(f);
+    end
+    g=255 - f;
 end
 
