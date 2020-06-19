5cd53c38ddaa9d2f04ad4678fb13534da14fda04
huerd <huerd@users.noreply.github.com>
2020-06-19 04:38:02 -0400

    - q1 done

--- dev02/section5/Q1/filtrerImageQ1.m
+++ dev02/section5/Q1/filtrerImageQ1.m
@@ -1,3 +1,9 @@
 function [imageFiltree] = filtrerImageQ1(image)
-imageFiltree = image;
+% Cette solution pour navette.jpg a une erreur quadratique moyenne de 58.6019
+imageFiltree = medfilt2(image,[5,5]);
+imageFiltree = medfilt2(imageFiltree,[2,2]);
+imageFiltree = medfilt2(imageFiltree,[3,3]);
+imageFiltree = medfilt2(imageFiltree,[1,1]);
+imageFiltree = medfilt2(imageFiltree,[3,3]);
+imageFiltree = medfilt2(imageFiltree,[5,5]);
 end
