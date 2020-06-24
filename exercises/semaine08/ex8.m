5a056792c2464d13f01602c5717bc181429a15de
huerd <2333486+huerd@users.noreply.github.com>
2020-06-23 20:35:35 -0400

    - in progress

--- exercises/semaine08/ex8.m
+++ exercises/semaine08/ex8.m
@@ -1,3 +1,11 @@
+imgBricks = imread("bricks.jpg");
+figure(1), imshow(imgBricks);
+imgCarpet = imread("carpet.jpg");
+figure(2), imshow(imgCarpet);
+
+lbpBricks = extractLBPFeatures(imgBricks, 'Upright', false);
+lbpCarpet = extractLBPFeatures(imgCarpet, 'Upright', false);
+
 if randi(2) == 1
 M = imread("bricks.jpg");
 else
@@ -11,4 +19,4 @@ y1 = randi(size(M,2)/2) + size(M,2)/2;
 M = M(x0 :x1, y0 :y1);
 M = imrotate(M, randi(5), "crop");
 
-imshow(M);
+figure(3), imshow(M);
